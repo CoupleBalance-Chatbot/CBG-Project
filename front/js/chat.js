73d87538ac988614815e8fcc0da18a0ab54dcdeb
jongnnis <ilovepjh0608@gmail.com>
2024-02-02 17:31:53 +0900

    chat창 마무리

--- front/js/chat.js
+++ front/js/chat.js
@@ -11,18 +11,44 @@ list.addEventListener('click', (e)=>{
 // 전송버튼 클릭
 const send = document.getElementById('send')
 send.addEventListener('click', (e)=> {
-    const input = document.getElementById('input').value
-    const chatting = document.getElementById('chatting')
-    const user = document.createElement('div')
+    // 전송 버튼 비활성화
+    send.disabled = true;
+    try {
+        const input = document.getElementById('input').value;
+        const chatting = document.getElementById('chatting');
+        const user = document.createElement('div');
+        
+        user.className = 'user';
+        user.innerHTML = `<div class="contentU">${input}</div>`;
+        
+        chatting.appendChild(user);
+        chatting.scrollTop = chatting.scrollHeight;
+        document.getElementById('input').value = '';
 
-    user.innerHTML = `<div class="contentU">${'한국인이라면 깻잎 잡아주는게 기본 아니야?'}</div>`
+        // const formData = {
+        //     input: input
+        // }
+        // const jsonData = JSON.stringify(formData)
+
+        // // AI에게 대답 받아오는 부분을 추가
+        // // input 보내기
+        // fetch('https://port-0-cbg-project-ghdys32blrzmx1y1.sel5.cloudtype.app/??????????????백엔드 url??????', {
+        //     method: "POST",
+        //     headers: {
+        //         "Content-Type": "application/json"
+        //     },
+        //     body: jsonData
+        // })
+        // .then(res => res.json())
+        // .then(data => {
+        //     // 받아온 data 처리하는 부분 innerHTML 해주기
+        // })
+
+        
+    } catch (error) {
+        console.error('에러 발생:', error);
+    } finally {
+        // 전송 버튼 다시 활성화
+        send.disabled = false;
+    }
 })
-
-
-var newDiv = document.createElement('div');
-            
-            // 생성한 div에 내용 추가
-            newDiv.innerHTML = '새로운 HTML 요소';
-
-            // 해당 div에 새로운 div 요소 추가
-            document.getElementById('myDiv').appendChild(newDiv);
