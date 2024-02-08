7b783ca3f7aaae8ced4065a08d0a885861e53080
정명수 <117971016+zzzxxcc123@users.noreply.github.com>
2024-02-08 22:04:38 +0900

    Add files via upload

--- server/Python server/main.py
+++ server/Python server/main.py
@@ -13,6 +13,8 @@ import random
 from fastapi.middleware.cors import CORSMiddleware
 # model 불어오기
 from model.model import model_open, model_close
+# Json
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
@@ -81,18 +83,23 @@ async def get_one(id: str):
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"내부 서버 오류: {str(e)}")
 
-@app.post("/chat/{id}")
-async def get_question(request: Request, id: str):
+@app.post("/chat_no")
+async def get_question(request: Request):
     try:
         data = await request.json()
-        if id == 'ok':
-            result = model_open(data)
-        # 입력값 가져오기
-        elif id == 'no':
-            result = model_close(data)
+        result = model_open(data['input'])
         # 반환된 값 보내기
-        return result
+        return JSONResponse({'prediction': result}, status_code=200)
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"내부 서버 오류: {str(e)}")
     
-
+@app.post("/chat_ok")
+async def get_question(request: Request):
+    try:
+        data = await request.json()
+        print(data)
+        # result = model_close(data)
+        # # 반환된 값 보내기
+        # return JSONResponse({'prediction': result}, status_code=200)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"내부 서버 오류: {str(e)}")
